048459523abc45b61e8838b25aeb7a95d1ee467d
RantuchoM <martin.rantucho@gmail.com>
2023-09-19 16:28:28 -0300

    W02 - Learning

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,43 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2015;
+        job1._endYear = 2020;
+        /*Console.WriteLine(job1._company);
+        Console.WriteLine(job1._jobTitle);
+        Console.WriteLine(job1._startYear);
+        Console.WriteLine(job1._endYear);*/
+        //job1.DisplayJobDetails();
+        
+        Job job2 = new() //VSC suggested this as a shortened way of creating a new instance. I'm testing it here!
+        {
+            _company = "Apple",
+            _jobTitle = "Software Developer",
+            _startYear = 2019,
+            _endYear = 2023
+        };
+        /*Console.WriteLine("");
+        Console.WriteLine(job2._company);
+        Console.WriteLine(job2._jobTitle);
+        Console.WriteLine(job2._startYear);
+        Console.WriteLine(job2._endYear);*/
+        //job2.DisplayJobDetails();
+
+
+
+        Resume myResume = new Resume(); 
+        myResume._name = "Martín Rantucho";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        
+        myResume.Display();
     }
 }
---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+
+public class Resume
+{
+    public string _name;
+    public List<Job> _jobs = new List<Job>();
+    
+    // Method to add a job to the resume
+    public void AddJob(Job job)
+    {
+        _jobs.Add(job);
+    }
+
+    // Method to display resume details
+    public void Display()
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine("Jobs:");
+        foreach (Job job in _jobs)
+        {
+            job.DisplayJobDetails();
+        }
+    }
+}
