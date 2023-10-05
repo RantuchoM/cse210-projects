d0cef690c98d36eb121570bee20f6ccf0ebabad1
RantuchoM <martin.rantucho@gmail.com>
2023-10-05 01:40:50 -0300

    W04 - Learning

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment1 = new("Martín Rantucho","Programming with Classes");
+        Console.WriteLine(assignment1.GetSummary());
+        Console.WriteLine();
+
+        MathAssignment mathAssign = new("Juan Pérez","This Topic","9.3","13.3");
+        Console.WriteLine(mathAssign.GetSummary());
+        Console.WriteLine(mathAssign.GetHomeworkList());
+        Console.WriteLine();
+        
+        WritingAssignment wriAssign = new("Dora Ríos","Another Topic","This is a new title");
+        Console.WriteLine(wriAssign.GetSummary());
+        Console.WriteLine(wriAssign.GetWritingInformation());
     }
 }
